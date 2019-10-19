4db9eda33079355cdef34288584ea96a0290dd2c
Kevin Wall <kwall@sci.utah.edu>
2019-10-19 11:27:02 -0600

    Data based game balance
    This change adds two new files: Balance.cs and the data file that drives it, balance.json.
    
    These files are intended to connect the arbitrary numbers associated with the implementation of all game elements to a single human readable file. For example, does cell construction require 4 units of structate, or 5? Does it consume 1 smole or NRG, or 0.75? etc.
    
    This enables fast iteration of low-code-impact gameplay changes, as well as limited modding potential. It also means that these sort of changes are centralized to one file, instead of being scattered across the entire codebase.
    
    This commit does not actually make the relevant classes use Balance.cs. I'm breaking that up into separate commits.

---
+++ Assets/Balance.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+public static class Balance
+{
+    public static class Actions//****confusion over reuse of terminology?
+    {
+        public static class Reaction
+        {
+            public static float Cost { get; set; }
+
+            static Reaction() { Load(); }
+        }
+
+        public static class Transcription
+        {
+            public static float Cost { get; set; }
+            public static float EnergyChange { get; set; }//****better term?
+
+            public static float UnitLength { get; set; }
+            public static float InterpretaseCostMultiplier { get; set; }
+
+            static Transcription() { Load(); }
+        }
+
+        public static class CompoundMovement//****Clunky, over-technical name
+        {
+            public static float Cost { get; set; }
+            public static float EnergyChange { get; set; }
+
+            static CompoundMovement() { Load(); }
+        }
+
+        public static class MembraneTransport
+        {
+            public static float Cost { get; set; }
+            public static float EnergyChange { get; set; }
+
+            public static Dictionary<Molecule, float> RateMultipliers { get; set; }
+
+            static MembraneTransport() { Load(); }
+        }
+
+        public static class CellConstruction
+        {
+            public static float Cost { get; set; }
+            public static float EnergyChange { get; set; }
+
+            public static Dictionary<Molecule, float> Materials { get; set; }
+
+            static CellConstruction() { Load(); }
+        }
+
+        public static class CellSeparation
+        {
+            public static float Cost { get; set; }
+            public static float EnergyChange { get; set; }
+
+            public static float Endowment { get; set; }
+
+            static CellSeparation() { Load(); }
+        }
+
+        public static class Grabbing
+        {
+            public static float Cost { get; set; }
+            public static float EnergyChange { get; set; }
+
+            static Grabbing() { Load(); }
+        }
+
+        public static class Spinning
+        {
+            public static float Cost { get; set; }
+            public static float EnergyChange { get; set; }
+
+            static Spinning() { Load(); }
+        }
+    }
+
+
+    static bool is_loaded = false;
+
+    public static void Load()
+    {
+        if (is_loaded)
+            return;
+        is_loaded = true;
+
+        JObject json_balance_object = JObject.Parse(FileUtility.ReadTextFile("Assets/balance.json"));
+
+        float cost_multiplier = Utility.JTokenToFloat(json_balance_object["Actions"]["Cost Multiplier"]);
+
+        Actions.Reaction.Cost = 1 * cost_multiplier;
+
+        Actions.Transcription.Cost = Utility.JTokenToFloat(json_balance_object["Actions"]["Transcription"]["Cost"]) * cost_multiplier;
+        Actions.Transcription.EnergyChange = Utility.JTokenToFloat(json_balance_object["Actions"]["Transcription"]["Energy Change"]);
+        Actions.Transcription.UnitLength = Utility.JTokenToFloat(json_balance_object["Actions"]["Transcription"]["Unit Length"]);
+        Actions.Transcription.InterpretaseCostMultiplier = Utility.JTokenToFloat(json_balance_object["Actions"]["Transcription"]["Interpretase Cost Multiplier"]);
+
+        Actions.CompoundMovement.Cost = Utility.JTokenToFloat(json_balance_object["Actions"]["Compound Movement"]["Cost"]) * cost_multiplier;
+        Actions.CompoundMovement.EnergyChange = Utility.JTokenToFloat(json_balance_object["Actions"]["Compound Movement"]["Energy Change"]);
+
+        Actions.MembraneTransport.Cost = Utility.JTokenToFloat(json_balance_object["Actions"]["Membrane Transport"]["Cost"]) * cost_multiplier;
+        Actions.MembraneTransport.EnergyChange = Utility.JTokenToFloat(json_balance_object["Actions"]["Membrane Transport"]["Energy Change"]);
+        Actions.MembraneTransport.RateMultipliers = new Dictionary<Molecule, float>();
+        foreach (var json_pair in json_balance_object["Actions"]["Membrane Transport"]["Rate Multipliers"] as JObject)
+            Actions.MembraneTransport.RateMultipliers[Molecule.GetMolecule(json_pair.Key)] = Utility.JTokenToFloat(json_pair.Value);
+
+        Actions.CellConstruction.Cost = Utility.JTokenToFloat(json_balance_object["Actions"]["Cell Construction"]["Cost"]) * cost_multiplier;
+        Actions.CellConstruction.EnergyChange = Utility.JTokenToFloat(json_balance_object["Actions"]["Cell Construction"]["Energy Change"]);
+        Actions.CellConstruction.Materials = new Dictionary<Molecule, float>();
+        foreach (var json_pair in json_balance_object["Actions"]["Cell Construction"]["Materials"] as JObject)
+            Actions.CellConstruction.Materials[Molecule.GetMolecule(json_pair.Key)] = Utility.JTokenToFloat(json_pair.Value);
+
+        Actions.CellSeparation.Cost = Utility.JTokenToFloat(json_balance_object["Actions"]["Cell Separation"]["Cost"]) * cost_multiplier;
+        Actions.CellSeparation.EnergyChange = Utility.JTokenToFloat(json_balance_object["Actions"]["Cell Separation"]["Energy Change"]);
+        Actions.CellSeparation.Endowment = Utility.JTokenToFloat(json_balance_object["Actions"]["Cell Separation"]["Endowment"]);
+
+        Actions.Grabbing.Cost = Utility.JTokenToFloat(json_balance_object["Actions"]["Grabbing"]["Cost"]) * cost_multiplier;
+        Actions.Grabbing.EnergyChange = Utility.JTokenToFloat(json_balance_object["Actions"]["Grabbing"]["Energy Change"]);
+
+        Actions.Spinning.Cost = Utility.JTokenToFloat(json_balance_object["Actions"]["Spinning"]["Cost"]) * cost_multiplier;
+        Actions.Spinning.EnergyChange = Utility.JTokenToFloat(json_balance_object["Actions"]["Spinning"]["Energy Change"]);
+    }
+}
